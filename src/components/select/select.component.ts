4b94941edc5e64d93d128f168ef740cb47e848b5
Mr.P <ssjblue197@gmail.com>
2025-03-26 09:44:36 +0700

    update select

--- src/components/select/select.component.ts
+++ src/components/select/select.component.ts
@@ -771,7 +771,7 @@ export default class PSelect extends PureElement implements PureFormControl {
   @watch("value", { waitUntilFirstUpdate: true })
   handleValueChange() {
     const allOptions = this.getAllOptions();
-    const value = Array.isArray(this.value) ? this.value : [this.value];
+    const value = Array.isArray(this.value) ? this.value : this.value ? [this.value] : [];
 
     if (value.length > 0) {
       this.displayInput.setAttribute("placeholder", "");
